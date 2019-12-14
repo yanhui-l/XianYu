773d83dff66cfa5efc44537409e7b912c0387c57
daxiong3 <2762722103>
2019-12-14 10:02:56 +0800

    第一次上传

---
+++ src/main/java/com/example/Market/controller/DomeController.java
@@ -0,0 +1,14 @@
+package com.example.Market.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DomeController {
+
+    @RequestMapping(value = "/Login")
+    public String Login(){
+        return "Login";
+    }
+}
---
+++ src/test/java/com/example/Market/MarketApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.Market;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
